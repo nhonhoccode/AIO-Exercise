bddce88066aebab1a2ad7efc2441e1c444ed49cd
nhonhoccode <trongnhon29032004@gmail.com>
2024-06-17 23:04:29 +0700

    Mod01/W02

--- mod01_w01/ex03_loss_function.py
+++ mod01_w01/ex03_loss_function.py
@@ -1,30 +1,34 @@
 import random
 import math
 
+
 def calculate_mae(targets, predictions):
     n = len(targets)
     mae = sum(abs(y - y_hat) for y, y_hat in zip(targets, predictions)) / n
     return mae
+
 
 def calculate_mse(targets, predictions):
     n = len(targets)
     mse = sum((y - y_hat) ** 2 for y, y_hat in zip(targets, predictions)) / n
     return mse
 
+
 def calculate_rmse(targets, predictions):
     mse = calculate_mse(targets, predictions)
     rmse = math.sqrt(mse)
     return rmse
 
+
 def main():
     num_samples = input("Nhập số lượng samples (num_samples): ")
-    
+
     if not num_samples.isnumeric():
         print("number of samples must be an integer number")
         return
 
     num_samples = int(num_samples)
-    
+
     loss_name = input("Nhập tên loss function (MAE, MSE, RMSE): ")
 
     predictions = [random.uniform(0, 10) for _ in range(num_samples)]
@@ -42,8 +46,10 @@ def main():
 
     print(f"loss name: {loss_name}")
     for i in range(num_samples):
-        print(f"sample-{i}: predict = {predictions[i]:.2f}, target = {targets[i]:.2f}")
+        print(
+            f"sample-{i}: predict = {predictions[i]:.2f}, target = {targets[i]:.2f}")
     print(f"loss: {loss:.2f}")
+
 
 if __name__ == "__main__":
     main()
